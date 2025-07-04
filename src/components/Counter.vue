9b68923592d07373b5cd2a0360856643f93efc14
MohanManirul <fiforeg@gmail.com>
2025-07-04 19:47:36 +0600

    global state

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+
+import {useCounterStore} from "../stores/counter"
+const store = useCounterStore();
+
+</script>
+
+<template>
+<p>I'm from Counter Component & My State Value Is : {{store.count}}</p>
+</template>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <script setup>
 
-import {useCounterStore} from "../stores/counter"
-const store = useCounterStore();
+
+
+import Counter from "../components/Counter.vue"
+
+
 </script>
 
 <template>
       <div class="container py-5 text-center">
-        <h2>🏠 Home Page</h2>
+        <h2>🏠 Home Page</h2>        
         <p>This is the home page of our Vue app using Bootstrap.</p>
+        <Counter />
       </div>
 </template>
 
