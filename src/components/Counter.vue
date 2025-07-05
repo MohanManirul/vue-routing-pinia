e822341190c475d78f8c64f9a3c909d9436c3fb9
MohanManirul <fiforeg@gmail.com>
2025-07-05 10:16:33 +0600

    df

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,14 +1,27 @@
-
 <script setup>
-
-import {useCounterStore} from "../stores/counter"
+import { computed } from "vue";
+import { useCounterStore } from "../stores/counter";
 const store = useCounterStore();
+
+function deccrement(){
+    store.count--
+}
+
+function increment(){
+    store.count++
+}
+
+
 
 </script>
 
 <template>
-<p>I'm from Counter Component & My State Value Is : {{store.count}}</p>
+  <div class="d-flex justify-content-center align-items-center gap-3">
+    <button class="btn btn-danger" @click="deccrement">-</button>
+    <p>Count Value: {{ store.count }}</p>
+    <button class="btn btn-primary"  @click="increment">+</button>
+  </div>
+  <p>Counter Moved To : {{ store.countDigitLength }}</p>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
